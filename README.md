d0eefe978f538967c5c0b289fd9b9bfb5d666405
JunIl-git <64887129+JunIl-git@users.noreply.github.com>
2020-10-30 01:04:57 +0900

    practice

--- README.md
+++ README.md
@@ -10,4 +10,11 @@ React JS Fundamentals Course (2020 Update!);
 
 https://github.com/JunIl-git/movie_app_2020
 
-End
+
+<h1>React 기초 정리</h1>
+
+<h5>component란?</h5>
+<p>HTML을 반환하는 함수이다.</p>
+
+<h5>JSX란?</h5>
+<p>js의 확장된 문법이다. React의 "element"를 생성한다.<p>
--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Navigation from "./components/Navigation";
-import Detail from './routes/Detail';
-import './App.css';
+
+function Food(props){
+  console.log(props)
+  return(
+    <div>
+      <h1>{props.fav}</h1>
+    </div>
+  )
+}
 
 function App(){
   return (
-    <HashRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home}/>
-      <Route  path="/about" component={About}/>
-      <Route path="/movie/:id" component={Detail}/>
-    </HashRouter>
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" 
+            something={true}></Food>
+    </div>
   )
 }
 
