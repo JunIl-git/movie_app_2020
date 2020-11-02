c67b3e85582b74e09a7d3c844a04e8b7bcfa22e9
JunIl-git <64887129+JunIl-git@users.noreply.github.com>
2020-11-02 22:30:21 +0900

    propTypes

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ React JS Fundamentals Course (2020 Update!);
 
 라우터와 라우트를 통한 페이지 이동을 공부했습니다.
 
+사용 API : YTS ("https://yts-proxy.now.sh/list_movies.json")
+
 https://github.com/JunIl-git/movie_app_2020
 
 
@@ -18,3 +20,35 @@ https://github.com/JunIl-git/movie_app_2020
 
 <h5>JSX란?</h5>
 <p>js의 확장된 문법이다. React의 "element"를 생성한다.<p>
+
+<h5>axios</h5>
+<p>HTTP 통신 라이브러리이다</p>
+<p>download => npm i axios</p>
+
+<h5>async / await</h5>
+<p>async/await는 비동기 처리 방식으로,
+async는 프로미스를 반환시키며 요청값이 나올 때 까지 대기시킨다.
+비동기를 동기처럼 사용
+
+1. 기본적인 코드 사용
+function fetchUser(){
+    //do network request in 10 secs.... //10초 정도 걸리는 요청값
+    return 'junil';
+}
+
+const user = fetchUser();       //
+console.log(user);              //user는 10초 정도 걸리기 때문에 값이 없음 error
+
+2. async/await 코드 사용
+async function fetchUser(){     //async와 await을 사용하여 요청값을 받을 떄까지 대기시킴
+    await axios.get("url");
+}
+
+const user = fetchUser();       //요청값을 다 받았으면 실행
+console.log(user)               //정상적 결과 출력
+</p>
+
+ <h5>PropTypes</h5>
+ <p>property들의 type을 체크 해준다.
+    download = npm i prop-types
+    import PropTypes from "prop-types"</p>
--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function Food(props){
-  console.log(props)
-  return(
-    <div>
-      <h1>{props.fav}</h1>
-    </div>
-  )
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoading : true,
+      movies : [],
+    }
+  }
+  getMovies = async () => {
+    const Movies = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    const arrayMovies = Movies.data.data.movies;
+    this.setState({
+      movies : arrayMovies,
+      isLoading : false,
+    })
+
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.isLoading ? "Loading..." : this.state.movies.map(movie =>{
+          return <Movie id={movie.id} 
+                        title={movie.title} 
+                        summary={movie.summary} 
+                        year={movie.year} 
+                        poster={movie.medium_cover_image}>
+                 </Movie>
+        })}
+      </div>
+    )
+  }
 }
-
-function App(){
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" 
-            something={true}></Food>
-    </div>
-  )
-}
-
 export default App;
